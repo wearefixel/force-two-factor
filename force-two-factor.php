13f7c8eaf0e2a5e72bf8629200175622df8d1ca6
Galen Gidman <galen@galengidman.com>
2024-05-29 13:00:06 -0500

    bump version

--- force-two-factor.php
+++ force-two-factor.php
@@ -4,7 +4,7 @@
  * Plugin Name: Force Two Factor
  * Plugin URI: https://github.com/wearefixel/force-two-factor
  * Description: Forces email-based two-factor authentication for all users unless another provider is enabled.
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Fixel
  * Author URI: https://wearefixel.com
  * Requires Plugins: two-factor
